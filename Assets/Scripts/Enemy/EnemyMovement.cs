3c38670ca70db2686b50dec5c993c866510ece09
aledifon <aledifon89@gmail.com>
2024-12-09 21:55:04 +0100

    Implemented Enemy Movement & Attack scripts by using Nav Mesh Agent Component

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        if (player == null)
+            return;
+        else
+            agent.SetDestination(player.transform.position);
+    }
+}
